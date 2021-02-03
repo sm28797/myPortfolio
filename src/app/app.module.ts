12c41b99ca3d5ead48f6b87918db08e16bccd2dd
Shubhanshu Mishra <shubhanshu.mishra13@gmail.com>
2021-02-03 20:40:32 +0530

    Added components and modified home.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ArComponent } from './ar/ar.component';
+import { ContactComponent } from './contact/contact.component';
+import { DevopsComponent } from './devops/devops.component';
+import { ServicesComponent } from './services/services.component';
+import { UiComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ArComponent,
+    ContactComponent,
+    DevopsComponent,
+    ServicesComponent,
+    UiComponent
   ],
   imports: [
     BrowserModule,
