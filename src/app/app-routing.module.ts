c7a1ccee67921cfb246c8274304c60eadbc7c0d7
Shubhanshu Mishra <shubhanshu.mishra13@gmail.com>
2021-02-05 14:09:20 +0530

    added the ui changes to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import {ServicesComponent} from './services/services.component';
+import {DevopsComponent} from './devops/devops.component';
+import {ArComponent} from './ar/ar.component';
+import {UiComponent} from './ui/ui.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `login component`
-  {path:"home",component:HomeComponent}
+  {path:"home", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"contact", component:ContactComponent},
+  {path:"services", component:ServicesComponent},
+  {path:"devops", component:DevopsComponent},
+  {path:"ar", component:ArComponent},
+  {path:"ui", component:UiComponent}
 ];
 
 @NgModule({
